f090f68602bcef152c33802e52b9e8d7639161e2
SteffenLee <tnqjae@naver.com>
2024-05-29 15:19:40 +0900

    Edit code(Add button)

--- lib/AboutKiosk/KioskInfo.dart
+++ lib/AboutKiosk/KioskInfo.dart
@@ -25,7 +25,8 @@ class KioskInfo extends StatelessWidget {
                   onPressed: () {},
                   child: Text('음식점',
                     style: TextStyle(
-                        fontSize: 30
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
                     ),),
                   style: ElevatedButton.styleFrom(
                     minimumSize: Size(150, 200),
@@ -38,7 +39,8 @@ class KioskInfo extends StatelessWidget {
                   onPressed: () {},
                   child: Text('카페',
                     style: TextStyle(
-                        fontSize: 30
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
                     ),),
                   style: ElevatedButton.styleFrom(
                     minimumSize: Size(150, 200),
@@ -51,25 +53,43 @@ class KioskInfo extends StatelessWidget {
             ),
           ),
           Padding(
-            padding: const EdgeInsets.symmetric(vertical: 50.0),
-            child: Center(
-              child: ElevatedButton(
-                onPressed: () {},
-                child: Text('무인민원발급기',
-                  style: TextStyle(
-                      fontSize: 20
-                  ),),
-                style: ElevatedButton.styleFrom(
-                  minimumSize: Size(150, 200),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(0),
+            padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text('무인민원발급기',
+                    style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                    ),),
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: Size(150, 200),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(0),
+                    ),
                   ),
                 ),
-              ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text('KTX / 열차',
+                    style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                    ),),
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: Size(150, 200),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(0),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
           Padding(
-            padding: const EdgeInsets.all(50.0),
+            padding: const EdgeInsets.all(100.0),
             child: Text(
               '언제나 당신 곁에 함께',
               style: TextStyle(fontSize: 15),
