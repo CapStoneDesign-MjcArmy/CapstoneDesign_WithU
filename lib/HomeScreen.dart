db5cbd5e8c7683cb14279605d44d58c980e2b5f1
SteffenLee <tnqjae@naver.com>
2024-05-29 10:06:24 +0900

    Commit

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -1,29 +1,29 @@
-import 'package:flutter/material.dart';
-import 'dart:async';
+  import 'package:flutter/material.dart';
+  import 'dart:async';
 
-class HomeScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'WithU',
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
+  class HomeScreen extends StatelessWidget {
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'WithU',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          bㅎackgroundColor: Color(0xFFACCA69),
+        ),
+        body: Align(
+          alignment: Alignment.bottomCenter, // 화면 하단에 배치
+          child: Padding(
+            padding: const EdgeInsets.all(50.0), // 여백 추가
+            child: Text(
+              '언제나 당신 곁에 함께',
+              style: TextStyle(fontSize: 15),
+            ),
           ),
         ),
-        backgroundColor: Color(0xFFACCA69),
-      ),
-      body: Align(
-        alignment: Alignment.bottomCenter, // 화면 하단에 배치
-        child: Padding(
-          padding: const EdgeInsets.all(50.0), // 여백 추가
-          child: Text(
-            '언제나 당신 곁에 함께',
-            style: TextStyle(fontSize: 24),
-          ),
-        ),
-      ),
-    );
+      );
+    }
   }
-}
