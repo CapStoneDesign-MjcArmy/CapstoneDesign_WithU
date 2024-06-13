7f98f95f51beeaae3b208ee55ef2ddcdef05696f
SteffenLee <tnqjae@naver.com>
2024-06-13 09:35:38 +0900

    Edit error

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:withu/Main/main.dart';
+import 'package:withu/Main/etc/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
